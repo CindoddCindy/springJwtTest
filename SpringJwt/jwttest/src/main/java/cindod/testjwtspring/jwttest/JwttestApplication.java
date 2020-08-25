fdffdd24c509aa2f3b596569d4edc92b730acc95
CindoddCindy <cindodcindy@gmail.com>
2020-08-25 18:52:20 +0700

    baru folder isi maven

---
+++ SpringJwt/jwttest/src/main/java/cindod/testjwtspring/jwttest/JwttestApplication.java
@@ -0,0 +1,13 @@
+package cindod.testjwtspring.jwttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwttestApplication.class, args);
+	}
+
+}
